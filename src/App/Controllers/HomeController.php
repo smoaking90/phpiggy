bb077325d8c28303004b4141da77501476f2e870
smoaking90 <cwsmoak90@gmail.com>
2023-10-30 21:25:12 +0100

    template engine
    added template engine class for eventually rendering HTML

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -1,13 +1,22 @@
 <?php
 
-declare(strict_types= 1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
     public function home()
     {
+        dd($this->view);
         echo "home page";
     }
 }
