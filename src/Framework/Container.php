af35783531418ed3d8d886f64229d51c58bef13c
smoaking90 <cwsmoak90@gmail.com>
2023-11-09 22:00:13 +0100

    invoked factory function
    no idea

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -50,8 +50,22 @@ class Container
             if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
                 throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
             };
+
+            $dependencies[] = $this->get($type->getName());
         }
 
-        dd($params);
+        dd($dependencies);
+    }
+
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exist in container");
+        }
+
+        $factory = $this->definitions[$id];
+        $dependency = $factory();
+
+        return $dependency;
     }
 }
