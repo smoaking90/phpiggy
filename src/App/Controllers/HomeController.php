d68fd537ddfe8b34dc54a7ebb91cbdc3c1ac2583
smoaking90 <cwsmoak90@gmail.com>
2023-11-09 21:09:58 +0100

    getting constructor params
    using reflection class get constructor and params, to get the constructor and it's parameters from controller

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -9,11 +9,8 @@ use App\Config\Paths;
 
 class HomeController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
+    public function __construct(private TemplateEngine $view)
     {
-        $this->view = new TemplateEngine(Paths::VIEW);
     }
     public function home()
     {
--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -24,6 +24,18 @@ class Container
             throw new ContainerException("Class {$className} is not instantiable");
         }
 
-        dd($reflectionClass);
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        dd($params);
     }
 }
