fd89494c556c126fea987f413c1de9e8f8ff58ea
smoaking90 <cwsmoak90@gmail.com>
2023-11-12 21:06:28 +0100

    middleware interface
    implemented middleware interface

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
+use App\Middleware\TemplateDataMiddleware;
 
 function registerMiddleware(App $app)
 {
+    $app->addMiddleware(TemplateDataMiddleware::class);
 }
