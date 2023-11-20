21d263fdfb30a5ba3c0af78451e2e7b86f24cfaf
smoaking90 <cwsmoak90@gmail.com>
2023-11-20 21:01:16 +0100

    validator service
    added validator service, container definition and injected it to auth controller

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -3,8 +3,10 @@
 declare(strict_types=1);
 
 use Framework\TemplateEngine;
+use App\Services\ValidatorService;
 use App\Config\Paths;
 
 return [
-    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW)
+    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
+    ValidatorService::class => fn () => new ValidatorService()
 ];
